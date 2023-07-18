62bc7ff37d2a0866256d3d3d6e86dd2129a29eed
Harsha <c.harsha0909@gmail.com>
2023-07-18 23:37:02 +0530

    Initial Commit

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -8,169 +8,187 @@ import { reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 import {
-  AIPicker,
-  FilePicker,
-  CustomButton,
-  ColorPicker,
-  Tab,
+    AIPicker,
+    FilePicker,
+    CustomButton,
+    ColorPicker,
+    Tab,
 } from "../components";
 
+const backendURL = import.meta.env.VITE_URL;
+
 const Customizer = () => {
-  const snap = useSnapshot(state);
-  const [file, setFile] = useState("");
-  const [prompt, setPrompt] = useState("");
-  const [generatingImg, setGeneratingImg] = useState(false);
+    const snap = useSnapshot(state);
+    const [file, setFile] = useState("");
+    const [prompt, setPrompt] = useState("");
+    const [generatingImg, setGeneratingImg] = useState(false);
 
-  const [activeEditorTab, setActiveEditorTab] = useState("");
-  const [showActiveEditorTab, setShowActiveEditorTab] = useState(false);
-  const [activeFilterTab, setActiveFilterTab] = useState({
-    logoShirt: true,
-    stylishShirt: false,
-  });
+    const [activeEditorTab, setActiveEditorTab] = useState("");
+    const [showActiveEditorTab, setShowActiveEditorTab] = useState(false);
+    const [activeFilterTab, setActiveFilterTab] = useState({
+        logoShirt: true,
+        stylishShirt: false,
+    });
 
-  const generateTabContent = () => {
-    switch (activeEditorTab) {
-      case "colorpicker":
-        return <ColorPicker />;
-      case "filepicker":
-        return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
-      case "aipicker":
-        return <AIPicker prompt={prompt} setPrompt={setPrompt} generatingImg={generatingImg} handleSubmit={handleSubmit}/>;
-      default:
-        return null;
-    }
-  };
-
-  const handleSubmit=async(type)=>{
-    if(!prompt) return alert("Please enter a prompt");
-    try{
-        setGeneratingImg(true);
-
-        const response = await fetch(config.production.backendUrl, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt }),
-        });
-
-        const data=await response.json();
-        if(response.status===200){
-            handleDecals(type,`data:image/png;base64,${data.photo}`);
+    const generateTabContent = () => {
+        switch (activeEditorTab) {
+            case "colorpicker":
+                return <ColorPicker />;
+            case "filepicker":
+                return (
+                    <FilePicker
+                        file={file}
+                        setFile={setFile}
+                        readFile={readFile}
+                    />
+                );
+            case "aipicker":
+                return (
+                    <AIPicker
+                        prompt={prompt}
+                        setPrompt={setPrompt}
+                        generatingImg={generatingImg}
+                        handleSubmit={handleSubmit}
+                    />
+                );
+            default:
+                return null;
         }
-    }catch(err){
-        alert(err)
-    }finally{
-        setGeneratingImg(false);
-        setActiveEditorTab("");
-    }
-  }
+    };
 
-  const handleActiveFilterTab = (tabName) => {
-    switch (tabName) {
-      case "logoShirt":
-        state.isLogoTexture = !activeFilterTab[tabName];
-        break;
-      case "stylishShirt":
-        state.isFullTexture = !activeFilterTab[tabName];
-        break;
-      default:
-        state.isFullTexture = false;
-        state.isLogoTexture = true;
-        break;
-    }
+    const handleSubmit = async (type) => {
+        if (!prompt) return alert("Please enter a prompt");
+        try {
+            setGeneratingImg(true);
 
-    //after setting the state, active FilterTab is updated
-    setActiveFilterTab((prevState) => {
-      return {
-        ...prevState,
-        [tabName]: !prevState[tabName],
-      };
-    });
-  };
+            const response = await fetch(backendURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ prompt }),
+            });
 
+            const data = await response.json();
+            if (response.status === 200) {
+                handleDecals(type, `data:image/png;base64,${data.photo}`);
+            }
+        } catch (err) {
+            alert(err);
+        } finally {
+            setGeneratingImg(false);
+            setActiveEditorTab("");
+            setPrompt("");
+        }
+    };
 
-  const handleDecals = (type, result) => {
-    const decalType = DecalTypes[type];
-    state[decalType.stateProperty] = result;
+    const handleActiveFilterTab = (tabName) => {
+        switch (tabName) {
+            case "logoShirt":
+                state.isLogoTexture = !activeFilterTab[tabName];
+                break;
+            case "stylishShirt":
+                state.isFullTexture = !activeFilterTab[tabName];
+                break;
+            default:
+                state.isFullTexture = false;
+                state.isLogoTexture = true;
+                break;
+        }
 
-    if (!activeFilterTab[decalType.filterTab]) {
-      handleActiveFilterTab(decalType.filterTab);
-    }
-  };
+        //after setting the state, active FilterTab is updated
+        setActiveFilterTab((prevState) => {
+            return {
+                ...prevState,
+                [tabName]: !prevState[tabName],
+            };
+        });
+    };
 
-  const readFile = (type) => {
-    reader(file).then((result) => {
-      handleDecals(type, result);
-      setActiveEditorTab("");
-    });
-  };
+    const handleDecals = (type, result) => {
+        const decalType = DecalTypes[type];
+        state[decalType.stateProperty] = result;
 
-  return (
-    <AnimatePresence>
-      {!snap.intro && (
-        <>
-          <motion.div
-            key="custom"
-            className="absolute top-0 left-0 z-10"
-            {...slideAnimation("left")}
-          >
-            <div className="flex items-center min-h-screen">
-              <div className="editortabs-container tabs">
-                {EditorTabs.map((tab) => (
-                  <Tab
-                    key={tab.name}
-                    tab={tab}
-                    handleClick={() => {
-                      if (
-                        activeEditorTab === tab.name ||
-                        (activeEditorTab !== tab.name &&
-                          showActiveEditorTab === false)
-                      ) {
-                        setShowActiveEditorTab((prevState) => !prevState);
-                      }
-                      setActiveEditorTab(tab.name);
-                    }}
-                  />
-                ))}
-                {showActiveEditorTab && generateTabContent()}
-              </div>
-            </div>
-          </motion.div>
-          <motion.div
-            className="absolute z-10 top-5 right-5"
-            {...fadeAnimation}
-          >
-            <CustomButton
-              type="filled"
-              title="Go Back"
-              handleClick={() => {
-                setShowActiveEditorTab(false);
-                state.intro = true;
-              }}
-              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-            />
-          </motion.div>
-          <motion.div
-            className="filtertabs-container"
-            {...slideAnimation("up")}
-          >
-            {FilterTabs.map((tab) => (
-              <Tab
-                key={tab.name}
-                tab={tab}
-                isFilterTab
-                isActiveTab={activeFilterTab[tab.name]}
-                handleClick={() => {
-                  handleActiveFilterTab(tab.name);
-                }}
-              />
-            ))}
-          </motion.div>
-        </>
-      )}
-    </AnimatePresence>
-  );
+        if (!activeFilterTab[decalType.filterTab]) {
+            handleActiveFilterTab(decalType.filterTab);
+        }
+    };
+
+    const readFile = (type) => {
+        reader(file).then((result) => {
+            handleDecals(type, result);
+            setActiveEditorTab("");
+        });
+    };
+
+    return (
+        <AnimatePresence>
+            {!snap.intro && (
+                <>
+                    <motion.div
+                        key="custom"
+                        className="absolute top-0 left-0 z-10"
+                        {...slideAnimation("left")}
+                    >
+                        <div className="flex items-center min-h-screen">
+                            <div className="editortabs-container tabs">
+                                {EditorTabs.map((tab) => (
+                                    <Tab
+                                        key={tab.name}
+                                        tab={tab}
+                                        handleClick={() => {
+                                            if (
+                                                activeEditorTab === tab.name ||
+                                                (activeEditorTab !== tab.name &&
+                                                    showActiveEditorTab ===
+                                                        false)
+                                            ) {
+                                                setShowActiveEditorTab(
+                                                    (prevState) => !prevState
+                                                );
+                                            }
+                                            setActiveEditorTab(tab.name);
+                                        }}
+                                    />
+                                ))}
+                                {showActiveEditorTab && generateTabContent()}
+                            </div>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="absolute z-10 top-5 right-5"
+                        {...fadeAnimation}
+                    >
+                        <CustomButton
+                            type="filled"
+                            title="Go Back"
+                            handleClick={() => {
+                                setShowActiveEditorTab(false);
+                                state.intro = true;
+                            }}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
+                    </motion.div>
+                    <motion.div
+                        className="filtertabs-container"
+                        {...slideAnimation("up")}
+                    >
+                        {FilterTabs.map((tab) => (
+                            <Tab
+                                key={tab.name}
+                                tab={tab}
+                                isFilterTab
+                                isActiveTab={activeFilterTab[tab.name]}
+                                handleClick={() => {
+                                    handleActiveFilterTab(tab.name);
+                                }}
+                            />
+                        ))}
+                    </motion.div>
+                </>
+            )}
+        </AnimatePresence>
+    );
 };
 
 export default Customizer;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import dalleRoutes from './routes/dalle.routes.js';
+import dalleRoutes from "./routes/dalle.routes.js";
 
 dotenv.config();
 
-const app=express();
-app.use(cors({
-    origin:process.env.ALLOWED_ORIGIN
-}));
-app.use(express.json({limit:"50mb"}));
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
-app.use('/api/v1/dalle',dalleRoutes);
-app.get('/',(req,res)=>{
-    res.status(200).json({message:'Hello from Dall-e'})
+app.use("/api/v1/dalle", dalleRoutes);
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello from Dall-e" });
 });
 
-app.listen(8080,()=>console.log('server has started on port 8080'))
+app.listen(8080, () => console.log("server has started on port 8080"));
